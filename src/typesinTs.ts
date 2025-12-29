8cdea44efa85eebee4858604c2185e55987551b9
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2025-12-29 15:28:48 +0530

    added typesinTs file

---
+++ src/typesinTs.ts
@@ -0,0 +1,10 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let channelName = "ChaiaurCode"
+
+let chaiFlavour:string = "masala chai"
+chaiFlavour = "Ginger tea"
+
+let chaiOrder:number;
