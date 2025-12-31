bfa75b5e2150a0f7fc165d062132052694270396
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2025-12-31 10:09:17 +0530

    added Generics.ts file

---
+++ src/Generics.ts
@@ -0,0 +1,31 @@
+function wrapInArray<T>(item: T): t[]{
+    return [item]
+}
+
+wrapInArray("Masala");
+wrapInArray(42)
+wrapInArray({flavor: "Ginger"})
+
+function pair<A, B>(a: A, b: B): [A, B]{
+    return[a, b]
+}
+
+pair("masala", 20)
+pair("masala", {flavor: "Ginger"})
+
+interface Box<T> {
+    content: T
+}
+
+const numberBox: Box<number> = {content: 10}
+const numberBoxCup: Box<string> = {content: "10"}
+
+interface ApiPromise<T>{
+    status: number,
+    data: T
+}
+
+const res: ApiPromise<{flavor: string}> = {
+    status: 200,
+    data: {flavor: "Masala"}
+}
